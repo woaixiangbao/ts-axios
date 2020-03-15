462e7cd635aea6663dec4814cc1f91ae00e52496
jianqing <blessjq@gmail.com>
2020-03-15 08:49:35 +0800

    feat(all the file): 处理headers
    在request中针对普通body添加合适的header，也就是content-type

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -75,3 +75,24 @@ axios({
   url: '/base/buffer',
   data: arr
 })
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,19 @@
 import { AxiosRequestConfig } from './types'
+import { head } from 'shelljs'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLocaleLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
