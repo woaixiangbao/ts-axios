cf062a5ec9b39ab258ddd4275442af5d66123282
jianqing <blessjq@gmail.com>
2020-03-15 17:00:12 +0800

    feat(all): Timeout or Newwork Error
    处理网络错误和超时错误

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,16 +2,23 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
 
     const request = new XMLHttpRequest()
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
     request.open(method.toUpperCase(), url, true)
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+      // 处理非200的状态码情况：网络错误、超时错误
+      if (request.status === 0) {
         return
       }
       const repsonseHeaders = request.getAllResponseHeaders()
@@ -24,8 +31,19 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
     }
+
+    //  网络错误处理
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+
+    // 超时处理
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+    }
+
     Object.keys(headers).forEach(name => {
       if (data === null && name.toLocaleLowerCase() === 'content-type') {
         delete headers[name]
@@ -34,5 +52,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed width status code ${response.status}`))
+      }
+    }
   })
 }
