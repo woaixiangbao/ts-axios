a7155aa9884d935454b7b10c84ff358832706a10
jianqing <blessjq@gmail.com>
2020-03-21 22:37:47 +0800

    feat(all): 配置项优化
    默认配置以及配置项合并

--- examples/server.js
+++ examples/server.js
@@ -37,6 +37,8 @@ registerExtendRouter()
 
 registerInterceptorRouter()
 
+registerConfigRouter()
+
 function registerSimpleRouter () {
   router.get('/simple/get', function(req, res) {
     console.log('(**********************)')
@@ -135,6 +137,12 @@ function registerExtendRouter () {
 function registerInterceptorRouter() {
   router.get('/interceptor/get', function(req, res) {
     res.end('hello')
+  })
+}
+
+function registerConfigRouter() {
+  router.post('/config/post', function(req, res) {
+    res.json(req.body)
   })
 }
 app.use(router)
--- src/types/index.ts
+++ src/types/index.ts
@@ -22,6 +22,7 @@ export interface AxiosRequestConfig {
   headers?: any
   responseType?: XMLHttpRequestResponseType
   timeout?: number
+  [propName: string]: any
 }
 
 export interface AxiosResponse<T = any> {
@@ -44,6 +45,7 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+  defaults: AxiosResponse
   interceptors: {
     request: AxiosInterceptorManager<AxiosRequestConfig>
     response: AxiosInterceptorManager<AxiosResponse>
